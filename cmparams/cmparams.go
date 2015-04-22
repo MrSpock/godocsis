45f0abb11cb3846dede186111a8e664610752427
Marcin 'Spock' Jurczuk <marcin@jurczuk.eu>
2015-04-22 15:46:55 +0200

    New binary cmparams from example addred to repo

---
+++ cmparams/cmparams.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/mrspock/godocsis"
+)
+
+func main() {
+	//var ip string
+	flag.Parse()
+	if len(flag.Args()) < 1 {
+		fmt.Println("Usage: cmparams <ip> <ip>")
+		return
+	}
+	s := godocsis.Session
+	for _, ip := range flag.Args() {
+		s.Target = ip
+		rs, err := godocsis.RFLevel(s)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Problem: %v", err)
+			//panic(err)
+		} else {
+			fmt.Printf("%s:", ip)
+			fmt.Printf("%.01f:", float32(rs.RF.USLevel[0])/10)
+			separator := ","
+			for no, ds := range rs.RF.DSLevel {
+				if no == rs.RF.DsBondingSize()-1 {
+					separator = ""
+				}
+				fmt.Printf("%.01f%v", float32(ds)/10, separator)
+			}
+			fmt.Println("")
+		}
+	}
+
+}
